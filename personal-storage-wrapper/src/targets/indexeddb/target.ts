2cbf84e7f0b11e0d515fadc3bb98fc57ce423443
Athenodoros <github@hmstoke.com>
2023-03-12 14:26:57 +0900

    Fix IDB target type

--- personal-storage-wrapper/src/targets/indexeddb/target.ts
+++ personal-storage-wrapper/src/targets/indexeddb/target.ts
@@ -15,9 +15,9 @@ const TABLE_NAME = "stores";
 export class IndexedDBTarget implements Target<IndexedDBTargetType, IndexedDBTargetSerialisationConfig> {
     type: IndexedDBTargetType = IndexedDBTargetType;
     private id: string;
-    private db: IDBDatabase;
+    private db: IDBDatabase | null;
 
-    private constructor(db: IDBDatabase, id: string | undefined) {
+    private constructor(db: IDBDatabase | null, id: string | undefined) {
         this.db = db;
 
         // Quick and dirty random 6-digit alpha-numeric string
@@ -25,7 +25,7 @@ export class IndexedDBTarget implements Target<IndexedDBTargetType, IndexedDBTar
     }
 
     // Async constructor
-    static create = async (id?: string): Promise<IndexedDBTarget | null> => {
+    static create = async (id?: string): Promise<IndexedDBTarget> => {
         const db = await new Promise<IDBDatabase | null>((resolve) => {
             if (!("indexedDB" in window)) return resolve(null);
 
@@ -39,12 +39,14 @@ export class IndexedDBTarget implements Target<IndexedDBTargetType, IndexedDBTar
             };
         });
 
-        return db && new IndexedDBTarget(db, id);
+        return new IndexedDBTarget(db, id);
     };
 
     // Data Handlers
     write = (buffer: ArrayBuffer): Result<Date> =>
         new Result((resolve) => {
+            if (this.db === null) return resolve({ type: "error" });
+
             const timestamp = new Date();
             const file: StoredIDBFile = { id: this.id, buffer, timestamp };
             const request = this.db.transaction(["stores"], "readwrite").objectStore("stores").put(file);
@@ -54,6 +56,8 @@ export class IndexedDBTarget implements Target<IndexedDBTargetType, IndexedDBTar
 
     read = (): Result<TargetValue> =>
         new Result((resolve) => {
+            if (this.db === null) return resolve({ type: "error" });
+
             const request = this.db.transaction(["stores"]).objectStore("stores").get(this.id);
             request.onsuccess = () => {
                 const result: StoredIDBFile | undefined = request.result;

4e83448da7ae624aade9c878a9255fd2b415c54e
Athenodoros <github@hmstoke.com>
2023-04-09 18:37:20 +0900

    Increase resiliency to races for listbuffer tests

--- personal-storage-wrapper/src/utilities/listbuffer/listbuffer.test.ts
+++ personal-storage-wrapper/src/utilities/listbuffer/listbuffer.test.ts
@@ -1,6 +1,8 @@
 import { expect, test } from "vitest";
 import { ListBuffer } from ".";
 
+const DELAY = 40;
+
 test("Correctly handles maximum length", () => {
     const buffer = new ListBuffer([2, 3, 4], { maxLength: 2 });
     expect(buffer.values()).toEqual([2, 3]);
@@ -10,33 +12,33 @@ test("Correctly handles maximum length", () => {
 });
 
 test("Correctly handles maximum duration", async () => {
-    const buffer = new ListBuffer([2, 3, 4], { maxMillis: 30 });
+    const buffer = new ListBuffer([2, 3, 4], { maxMillis: DELAY });
     expect(buffer.values()).toEqual([2, 3, 4]);
 
-    await wait(10);
+    await wait(DELAY * 0.5);
     buffer.push(5);
     buffer.push(6);
     expect(buffer.values()).toEqual([6, 5, 2, 3, 4]);
 
-    await wait(30);
+    await wait(DELAY);
     expect(buffer.values()).toEqual([6, 5]);
 
-    await wait(30);
+    await wait(DELAY);
     expect(buffer.values()).toEqual([]);
 });
 
 test("Correctly handles maximum duration and length", async () => {
-    const buffer = new ListBuffer([2, 3, 4], { maxMillis: 30, maxLength: 2 });
+    const buffer = new ListBuffer([2, 3, 4], { maxMillis: DELAY, maxLength: 2 });
     expect(buffer.values()).toEqual([2, 3]);
 
-    await wait(10);
+    await wait(DELAY * 0.5);
     buffer.push(5);
     expect(buffer.values()).toEqual([5, 2]);
 
-    await wait(30);
+    await wait(DELAY);
     expect(buffer.values()).toEqual([5]);
 
-    await wait(30);
+    await wait(DELAY);
     expect(buffer.values()).toEqual([]);
 });
 

43e2e42625cbab063baa0978897566d87aad3cf9
Athenodoros <github@hmstoke.com>
2023-05-14 20:00:04 +0900

    Update redirect URIs for new server root

--- personal-storage-wrapper-example/src/application/constants.ts
+++ personal-storage-wrapper-example/src/application/constants.ts
@@ -1,4 +1,4 @@
 export const DROPBOX_CLIENT_ID = "sha2xamq49ewlbo";
-export const DROPBOX_REDIRECT_URI = window.location.origin + "/dropbox";
+export const DROPBOX_REDIRECT_URI = window.location.origin + "/personal-storage-wrapper/dropbox";
 export const GDRIVE_CLIENT_ID = "151346048888-a4i2hah9aqh8bm4058muuau52sfcp0ge.apps.googleusercontent.com";
-export const GDRIVE_REDIRECT_URI = window.location.origin + "/gdrive";
+export const GDRIVE_REDIRECT_URI = window.location.origin + "/personal-storage-wrapper/gdrive";

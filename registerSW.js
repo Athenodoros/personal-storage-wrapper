50d847163b96d2c77adc5c911a01331c248412d9
GH Action <Athenodoros@noreply.github.com>
2024-10-16 07:19:22 +0000

    Package resources for gh-pages

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/personal-storage-wrapper/sw.js', { scope: '/personal-storage-wrapper/' })})}
